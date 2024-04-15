083c04c45e0b709d75c11a60bfa710e5b2b0cdca
Polaris81 <55464461+Polaris81@users.noreply.github.com>
2024-04-15 13:25:26 -0500

    Update views.py

--- final_Website/final_Website_Utilities/views.py
+++ final_Website/final_Website_Utilities/views.py
@@ -5,3 +5,15 @@ def index(request):
 
 def team(request):
     return HttpResponse("Team")
+    
+def about(request):
+    return HttpResponse("About")
+
+def faq(request):
+    return HttpResponse("Faq")
+
+def classes(request):
+    return HttpResponse("Classes")
+
+def contact(request):
+    return HttpResponse("Contact")
