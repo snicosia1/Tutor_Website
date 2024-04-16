a06fbf2f7f1c2388c75332dd1176a59f0ffc7fd6
OmareNas <erasmoscholar10@gmail.com>
2024-04-16 09:24:42 -0500

    Register n Login/logout
    Added a 'Sign In' button that display only when no user is logged in and switches to 'Log out' when a user is sign in. Also added a the login and log out html pages with a registering user feature in the sign in page.

--- final_Website/store/urls.py
+++ final_Website/store/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('tutor/', views.tutor, name='tutor'),
     path('class/', views.classes, name='class'),
-    path('faq', views.faq, name='faq'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 
 ]
--- final_Website/store/views.py
+++ final_Website/store/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 def home(request):
@@ -23,3 +29,45 @@ def about(request):
 def contact(request):
     return render(request,'contact.html',{})
 
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, try again!"))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Account created successfully')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was a problem registering')
+            return redirect('register')
+
+    else:
+        return render(request,'register.html',{'form':form})
